adad55f96a3ab76ccb132d6e82b1bb2151045440
minola <stanciuminola@yahoo.com>
2023-05-15 10:51:56 +0300

    primul commit de test, doar main

---
+++ main.c
@@ -0,0 +1,12 @@
+//link tema: https://ocw.cs.pub.ro/courses/sda-ab/tema1
+
+#include "functii.h"
+
+int main()
+{
+    TEAM* head = NULL;
+    createLIST(&head);
+    printf("heei\2\2\2\2\2\2");
+    return 0;
+}
+
