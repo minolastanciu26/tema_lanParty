3062984f506851cbafccb08b9eb19e28925357ad
minola <stanciuminola@yahoo.com>
2023-05-18 17:07:45 +0300

    main 1 si 2

--- lan-party-02-checker-main/main.c
+++ lan-party-02-checker-main/main.c
@@ -1,107 +1,97 @@
 // link tema: https://ocw.cs.pub.ro/courses/sda-ab/tema1
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-struct Player
-{
-    char* firstName;
-    char* secondName;
-    int points;
-};
-
-typedef struct Player PLAY;
-
-struct Team
-{
-    int nr_players;
-    char* teamName;
-    PLAY *p;
-    struct Team *next;
-};
-
-typedef struct Team TEAM;
-
-void createLIST(TEAM **head, FILE *input)
-{   
-    TEAM *newnode = (TEAM *)malloc(sizeof(TEAM));
-    int nr_players, points;
-    char teamName[50], firstName[50], secondName[50];
-    fscanf(input, "%d", &nr_players);
-    newnode->nr_players = nr_players;
-    fgetc(input);
-    fscanf(input, "%[^\n]", teamName);
-    fgetc(input);
-    newnode->teamName = (char *)malloc((strlen(teamName)+1) * sizeof(char));
-    strcpy(newnode->teamName, teamName);
-    newnode->p = (PLAY *)malloc((newnode->nr_players) * sizeof(PLAY));
-    for (int j = 0; j < newnode->nr_players; j++)
-    {
-        fscanf(input, "%s", firstName);
-        fgetc(input);
-        newnode->p[j].firstName = (char *)malloc((strlen(firstName)+1) * sizeof(char));
-        strcpy(newnode->p[j].firstName, firstName);
-        fscanf(input, "%s", secondName);
-        newnode->p[j].secondName = (char *)malloc((strlen(secondName)+1) * sizeof(char));
-        strcpy(newnode->p[j].secondName, secondName);
-        fscanf(input, "%d", &points);
-        newnode->p[j].points = points;
-        fgetc(input);
-    }
-    newnode->next = *head;
-    *head = newnode;
-}
+#include "header.h"
 
 int main(int argc, char *argv[])
-{   
-    FILE *c;
-    if ((c = fopen(argv[1], "rt")) == NULL)
-    {
-        printf("Eroare la deschiderea fisierului c!\n");
-        exit(1);
-    }
-    int tasks[5];
+{
+    //deschide fisierele
+    FILE *c = fopen(argv[1], "rt");
+    openFile(c);
+    FILE *d = fopen(argv[2], "rt");
+    openFile(d);
+    FILE *r = fopen(argv[3], "wt");
+    openFile(r);
+
+    //citeste continutul fisierului c.in
+    int tasks[6];
     for (int i = 0; i < 5; i++)
     {
         fscanf(c, "%d", &tasks[i]);
     }
-    
-    if (tasks[0] == 1)
-    {
-        FILE *input;
-        if ((input = fopen(argv[2], "rt")) == NULL)
-        {
-            printf("Eroare la deschiderea fisierului d!\n");
-            exit(1);
-        }
-        TEAM *head = (TEAM*)malloc(sizeof(TEAM));
-        head = NULL;
-        int nr_teams;
-        fscanf(input, "%d", &nr_teams);
-        for (int i = 0; i < nr_teams; i++)
-        {
-            createLIST(&head, input);
-            fgetc(input);
 
-        }
-        
-        FILE *rez;
-        if ((rez = fopen(argv[3], "wt")) == NULL)
+    //citeste cate echipe sunt in fisierul d.in si creeaza lista cu acestea
+    TEAM *head = (TEAM *)malloc(sizeof(TEAM));
+    head = NULL;
+    int nr_teams;
+    fscanf(d, "%d", &nr_teams);
+    for (int i = 0; i < nr_teams; i++)
+    {
+        createLIST(&head, d);
+        fgetc(d);
+    }
+
+    //rezolva task-ul 1, afisand numele echipelor in fisierul r.out
+    if (tasks[0] == 1 && tasks[1]==0)
+    {
+        TEAM *listHead = head;
+        for (int i = 0; i < nr_teams && listHead != NULL; i++)
         {
-            printf("Eroare la deschiderea fisierului rez!\n");
-            exit(1);
-        }
-        TEAM* listHead = head;
-        for (int i = 0; i < nr_teams && listHead!=NULL; i++)
-        {  
-            fprintf(rez, "%s\n", listHead->teamName);
+            fprintf(r, "%s\n", listHead->teamName);
             listHead = listHead->next;
         }
-        fclose(input); 
-        fclose(rez);
-        fclose(c);
     }
+
+    //rezolva task-ul 2
+    if (tasks[1] == 1 )
+    {
+        //calculeaza n maxim
+        int n = 1;
+        while (n * 2 < nr_teams)
+            n = n * 2;
+        
+        //calculeaza punctajul pentru fiecare echipa
+        TEAM *aux = (TEAM*)malloc(sizeof(TEAM));
+        aux = head;
+        for (int i = 0; i < nr_teams && aux != NULL; i++)
+        {
+            aux->teamPoints = 0;
+            for (int j = 0; j < aux->nr_players; j++)
+                aux->teamPoints = aux->teamPoints + aux->p[j].points;
+            aux->teamPoints = aux->teamPoints / (aux->nr_players *(1.0)); 
+            aux = aux->next;
+        }
+        //elimina echipa cu punctajul minim pana ajung sa fie n echipe
+        while (nr_teams > n && head!=NULL)
+        {
+            TEAM *head_copy = (TEAM*)malloc(sizeof(TEAM));
+            head_copy = head;
+            float mini = head_copy->teamPoints;
+            head_copy = head_copy->next;
+            for (int i = 0; i < nr_teams && head_copy != NULL; i++)
+            {
+                if (head_copy->teamPoints < mini)
+                    mini = head_copy->teamPoints;
+                head_copy = head_copy->next;
+            }
+            delete (&head, mini);
+            nr_teams--;
+            //free(head_copy);
+        }
+        //afiseaza numele echipelor dupa eliminare, in fisierul r.out 
+        TEAM *listHead = (TEAM*)malloc(sizeof(TEAM));
+        listHead = head;
+        for (int i = 0; i < nr_teams && listHead != NULL; i++)
+        {
+            fprintf(r, "%s\n", listHead->teamName);
+            listHead = listHead->next;
+        }
+        free(listHead);
+    }
+
+    //inchide fisierele
+    fclose(d);
+    fclose(r);
+    fclose(c);
 
     return 0;
 }
